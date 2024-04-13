fbb0125683d1da3592ee80469333510fe7c47b9c
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2024-04-13 10:25:54 +0200

    Develop (#19)
    * improved response pointers handling
    
    * group everything in a single class
    
    * use the latest common workflows
    
    * Use common cmake
    
    use Common cmake for cpack and documentation.
    Also, install dlls on bin and compiler libraries.

--- include/omclient.hpp
+++ include/omclient.hpp
@@ -4,6 +4,11 @@
 #include <QtCore/QtGlobal>
 #include <QNetworkReply>
 #include <QJsonValue>
+#include<QJsonObject>
+
+#if defined(USE_QML)
+#include <QtQml>
+#endif
 
 #if defined(WINDOWS_OMClient)
 # define OMClient_EXPORT Q_DECL_EXPORT
@@ -17,36 +22,70 @@ class OMClient_EXPORT OMResponse: public QObject
 public:
     OMResponse(QNetworkReply *thereply);
     void fill();
-    void error_found(QNetworkReply::NetworkError code);
+    void errorFound(QNetworkReply::NetworkError code);
 signals:
     void returned( QJsonValue data );
 private:
-    QNetworkReply *reply;
+    QNetworkReply *m_reply;
 
 };
 
 class OMClient_EXPORT OMClient: public QObject
 {
-
     Q_OBJECT
+#if defined(USE_QML)
+    Q_PROPERTY(QJsonObject  currentWeather READ currentWeather NOTIFY currentWeatherChanged)
+    Q_PROPERTY(float  latitude READ latitude WRITE setLatitude  NOTIFY positionChanged)
+    Q_PROPERTY(float  longitude READ longitude WRITE setLongitude NOTIFY positionChanged)
+    Q_PROPERTY(TempUnit  tempUnit MEMBER m_tempUnit NOTIFY tempUnitChanged)
+    Q_PROPERTY(QUrl  apiUrl MEMBER m_apiUrl NOTIFY apiUrlChanged)
+    QML_ELEMENT
+#endif
+
 public:
     OMClient(QObject *parent = nullptr);
-    enum ClientState {
-        Disconnected = 0,
-        Connected
+    Q_INVOKABLE void getCurrentWeather(void);
+    QJsonObject currentWeather()const {return m_currentWeather;}
+    float latitude()const{return m_latitude;}
+    float longitude()const{return m_longitude;}
+    void setLatitude(float lat){
+        if(lat!=m_latitude&&lat>=-90&&lat<=90)
+        {
+            m_latitude=lat;
+            emit positionChanged();
+        }
+    }
+    void setLongitude(float lon){
+        if(lon!=m_longitude&&lon>=-180&&lon<=180)
+        {
+            m_longitude=lon;
+            emit positionChanged();
+        }
+    }
+    enum TempUnit {
+        Celsius = 0,
+        Fahrenheit
     };
 
-    OMResponse*  get_reply_rest(const QString& path, const QString &query=QString())const;
+    Q_ENUM(TempUnit)
+
+
     void setAPIUrl(const QUrl apiUrl);
 
 signals:
-    void stateChanged();
+    void currentWeatherChanged();
+    void positionChanged();
+    void tempUnitChanged();
+    void apiUrlChanged();
 
 private:
-    void set_State(ClientState state_m){if(state_m!=state_){state_=state_m;emit stateChanged();}}
-    QUrl m_ApiUrl;
-    QNetworkAccessManager* nam;
-    ClientState state_;
+    OMResponse*  getReplyRest(const QString& path, const QString &query=QString())const;
+    QUrl m_apiUrl;
+    QNetworkAccessManager* m_nam;
+    QJsonObject m_currentWeather;
+    float m_latitude,m_longitude;
+    TempUnit  m_tempUnit;
+    static QHash<TempUnit, QString> tempUnitStr;
 };
 
 
